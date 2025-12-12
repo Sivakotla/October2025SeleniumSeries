bd80de8937523dcc8eea85f043d6a9b782fb790d
Siva <sivareddi.kotla@gmail.com>
2025-12-12 10:35:40 +0530

    added/updated/deleted

---
+++ src/main/java/SeleniumSessions/TestDemo.java
@@ -0,0 +1,11 @@
+package SeleniumSessions;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Demo");
+	}
+
+}
